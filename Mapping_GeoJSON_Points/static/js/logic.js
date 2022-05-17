267ac61afa42b11fe248a66c3d407a2fcede835c
luperrin <luke@lukeperrin.me>
2022-05-17 07:52:40 -0700

    adding torontoRoutes.json

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -22,7 +22,7 @@ let sanFranAirport =
 ]};
 
 // Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -30,6 +30,44 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 100,
     accessToken: API_KEY
 });
+
+// streets.addTo(map);
+
+  // We create the dark view tile layer that will be an option for our map.
+  let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
+  
+    // Create a base layer that holds both maps.
+  let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+  
+  // Create the map object with center, zoom level and default layer.
+  let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+  })
+  
+  // Pass our map layers into our layers control and add the layers control to the map.
+  L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/luperrin/13-Mapping-Earthquakes-lukeperrin/main/Mapping_GeoJSON_Points/static/js/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
+
+
 
 
 // Grabbing our GeoJSON data.
@@ -51,4 +89,4 @@ L.geoJSON(sanFranAirport, {
 });
 
 
-  streets.addTo(map);
+
