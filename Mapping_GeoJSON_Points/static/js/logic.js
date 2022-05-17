7f9ba40624be0478941ad2cbef6c3b21d8d8ef28
luperrin <luke@lukeperrin.me>
2022-05-17 08:07:18 -0700

    adding Mapping_GeoJSON_LineStrings

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -88,5 +88,23 @@ L.geoJSON(sanFranAirport, {
    }
 });
 
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/luperrin/13-Mapping-Earthquakes-lukeperrin/main/Mapping_GeoJSON_Points/static/js/torontoRoutes.json";
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
-
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+  }
+})
+.addTo(map);
+});
